cc460604a2dd51f68af92f0d77879bb935ef5d1a
Rodrigo Mayer <rodrigo-mayer@hotmail.com>
2022-05-08 19:19:18 -0300

    fixed language buttons

--- src/components/LangButton/LangButton.jsx
+++ src/components/LangButton/LangButton.jsx
@@ -6,16 +6,20 @@ function LangButton() {
 
     return (
         <li className='lang-button-container'  onMouseEnter={() => {setActive(true); console.log(isActive)}} onMouseLeave={() => {setActive(false)}}>
-            <img src="./flag.svg" alt="Flag Logo" className='flag-logo'/>
+            <img src="./flag-US.svg" alt="Flag Logo" className='flag-logo'/>
             <span className='lang-text'>US</span>
             <i className="fa-solid fa-angle-down arrow"></i>
             {isActive && (
                 <div className='list-langs-container'>
                     <div className='list-langs'>
-                        <img src="./flag.svg" alt="Flag Logo" className='flag-logo'/>
-                        <span className='lang-text'>US</span>
-                        <img src="./flag.svg" alt="Flag Logo" className='flag-logo'/>
-                        <span className='lang-text'>US</span>
+                        <div className="lang-container">
+                            <img src="./flag-BR.svg" alt="Flag Logo" className='flag-logo'/>
+                            <span className='lang-text'>BR</span>
+                        </div>
+                        <div className="lang-container">
+                            <img src="./flag-MX.svg" alt="Flag Logo" className='flag-logo'/>
+                            <span className='lang-text'>MX</span>
+                        </div>
                     </div>
                 </div>)}
         </li>
