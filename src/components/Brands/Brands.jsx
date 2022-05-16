92faf1076cbc87012ab33d96a53327f61c07ff07
Rodrigo Mayer <rodrigo-mayer@hotmail.com>
2022-05-16 19:35:58 -0300

    adjust media queries

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -4,13 +4,20 @@ function Brands() {
     return (
         <>
         <div className="brands-container container">
-            {/* <div className="blur left"></div>
-            <div className="blur right"></div> */}
-            <div className="brand-logo"><img src="./logo-embraer.png" alt="Brand Logo" className='logo-img'/></div>
-            <div className="brand-logo"><img src="./logo-john-deere.png" alt="Brand Logo" className='logo-img'/></div>
-            <div className="brand-logo"><img src="./logo-pirelli-2.png" alt="Brand Logo" className='logo-img'/></div>
-            <div className="brand-logo"><img src="./logo-suzano-2.png" alt="Brand Logo" className='logo-img'/></div>
-            <div className="brand-logo"><img src="./logo-arcelor-2.png" alt="Brand Logo" className='logo-img'/></div>
+            <div className="blur left"></div>
+            <div className="blur right"></div>
+            <div className="brands-wrapper">
+                {/* <div className="blur left"></div>
+                <div className="blur right"></div> */}
+                <div className="brand-logo"><img src="./logo-tigre-2.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-danone-2.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-embraer.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-john-deere.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-pirelli-2.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-suzano-2.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-arcelor-2.png" alt="Brand Logo" className='logo-img'/></div>
+                <div className="brand-logo"><img src="./logo-air-liquide-2.png" alt="Brand Logo" className='logo-img'/></div>
+            </div>
         </div>
         </>
     )
--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -3,7 +3,7 @@ import './StatCard.css';
 function StatCard(props) {
     return(
         <div className="stat-card-container">
-            <p className="stat-percentage">{props.perc}</p>
+            <p className="stat-percentage">{props.icon}{props.perc}</p>
             <p className="stat-desc">{props.desc}</p>
             <img src="./bluecap.png" alt="Bluecap" className='bluecap-img'/>
         </div>
